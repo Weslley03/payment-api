6e2ea41963a4f0ac7fe5d55fdd10730a06e4a55b
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-09-02 20:03:00 -0300

    feat: add handleValidatonsErros

--- src/main/java/com/weftecnologia/payment_api/exception/GlobalHandlerException.java
+++ src/main/java/com/weftecnologia/payment_api/exception/GlobalHandlerException.java
@@ -1,9 +1,11 @@
 package com.weftecnologia.payment_api.exception;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,21 @@ import com.weftecnologia.payment_api.exception.exceptions.UserAlreadyExistsExcep
 
 @RestControllerAdvice
 public class GlobalHandlerException {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> handleValidatonsErros(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult().getFieldErrors().forEach(err -> {
+      errors.put(err.getField(), err.getDefaultMessage());
+    });
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        Map.of(
+            "message", "body request validation error.",
+            "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
+            "errors", errors));
+  }
 
   @ExceptionHandler(UserAlreadyExistsException.class)
   public ResponseEntity<?> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
