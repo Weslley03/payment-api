90764bc81229d6753081e5cbcd58bc6a3851fe51
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-09-02 16:56:30 -0300

    fix: update response of AuthController

--- src/main/java/com/weftecnologia/payment_api/controller/AuthController.java
+++ src/main/java/com/weftecnologia/payment_api/controller/AuthController.java
@@ -1,12 +1,10 @@
 package com.weftecnologia.payment_api.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.weftecnologia.payment_api.dto.ResponseApiDTO;
 import com.weftecnologia.payment_api.dto.user.AuthUserDTO;
 import com.weftecnologia.payment_api.dto.user.ResponseAuthUserDTO;
 import com.weftecnologia.payment_api.repository.AuthRepository;
@@ -22,14 +20,7 @@ public class AuthController {
   }
 
   @PostMapping()
-  public ResponseApiDTO<ResponseAuthUserDTO> auth(@RequestBody AuthUserDTO dto) {
-    ResponseAuthUserDTO responseAuthUserDTO = authRepository.auth(dto);
-    if (!responseAuthUserDTO.isSuccess()) {
-      return new ResponseApiDTO<ResponseAuthUserDTO>(
-          "falha na autenticação.",
-          HttpStatus.BAD_REQUEST.value(),
-          responseAuthUserDTO);
-    }
-    return new ResponseApiDTO<ResponseAuthUserDTO>("logged user.", HttpStatus.OK.value(), responseAuthUserDTO);
+  public ResponseAuthUserDTO auth(@RequestBody AuthUserDTO dto) {
+    return authRepository.auth(dto);
   }
 }
