72b3b82f40142119872c96e9211b9c11b51c1df6
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-09-09 17:19:06 -0300

    fix: add @NotBlank in CreateOrderDTO

--- src/main/java/com/weftecnologia/payment_api/dto/order/CreateOrderDTO.java
+++ src/main/java/com/weftecnologia/payment_api/dto/order/CreateOrderDTO.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import com.weftecnologia.payment_api.dto.product.ProductDTO;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CreateOrderDTO {
+  @NotBlank
   private String paymentMethod;
+
+  @NotBlank
   private List<ProductDTO> products;
 }
