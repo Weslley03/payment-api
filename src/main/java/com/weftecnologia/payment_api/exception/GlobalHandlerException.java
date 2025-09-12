8a8dfb3184c9eb979f83011a32c0611a180e6bc0
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-09-11 21:26:23 -0300

    feat: add HttpMessageNotReadableException in GlobalHandlerException

--- src/main/java/com/weftecnologia/payment_api/exception/GlobalHandlerException.java
+++ src/main/java/com/weftecnologia/payment_api/exception/GlobalHandlerException.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -62,5 +63,13 @@ public class GlobalHandlerException {
         Map.of(
             "message", ex.getMessage(),
             "status", String.valueOf(HttpStatus.NOT_FOUND.value())));
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        Map.of(
+            "message", "Invalid or missing request body.",
+            "status", String.valueOf(HttpStatus.BAD_REQUEST.value())));
   }
 }
